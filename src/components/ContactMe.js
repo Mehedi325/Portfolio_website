537811dc3e07c2f10c1210473906c0c192ff95d8
Mehedi Hasan Masum <132672069+Mehedi325@users.noreply.github.com>
2023-08-02 17:34:27 -0600

    Update ContactMe.js

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -16,9 +16,7 @@ function ContactMe() {
   };
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
-        Contact
-      </h3>
+
 
       <div className="flex flex-col space-y-10 mt-10">
         <h4 className="text-2xl md:text-4xl font-semibold text-center">
