ce684925eb43e4028bf0b07dc5a7474f587fc4c2
Mehedi Hasan Masum <132672069+Mehedi325@users.noreply.github.com>
2023-08-03 08:21:05 -0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ import "./All.css";
 function About() {
   return (
     <div className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-7 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-10 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
       <motion.img
